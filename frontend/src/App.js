41649ec4f2e83f923f43e615cc86e6c33cbbd613
Your Name <email@example.com>
2021-10-09 00:09:39 -0700

    phase 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 
 function App() {
@@ -16,7 +17,10 @@ function App() {
       <Route path="/login">
         <LoginFormPage />
       </Route>
-      <Route path='/'>
+      <Route path="/signup">
+        <SignupFormPage />
+      </Route>
+      <Route exact path='/'>
         Main Page
       </Route>
     </Switch>
